89e85d07af844f8a05b603b1402cae5cf28dba79
dhchoi <dhchoi@grooo.co.jp>
2024-12-27 15:26:59 +0900

    #23. 엘로퀀트 스트릭트 모드

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 엘로퀀트 스트릭트 모드 : 레이지 로딩, 대량 할당, 존재하지 않는 변수들에 대한 에러를 잡아준다.
+        Model::shouldBeStrict(! $this->app->isProduction()); // prod에서는 동작하지 않도록 
     }
 }
