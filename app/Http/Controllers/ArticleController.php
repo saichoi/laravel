03ad84ab180c482dbfec4e51566e0cf7acc07ec6
dhchoi <dhchoi@grooo.co.jp>
2024-12-27 17:00:13 +0900

    #29. 컨트롤러 라우트 그룹

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+use Illuminate\Support\Facades\Auth;
+
+class ArticleController extends Controller
+{
+    public function create() {
+        return view('articles/create');
+    }
+
+    public function index(Request $request) {
+        $input = $request->validate([
+            'body' => [
+                'required',
+                'string', 
+                'max:255',
+            ],
+        ]);
+    
+        Article::create([
+            'body' => $input['body'],
+            'user_id' => Auth::id()
+        ]);
+        
+        return redirect()->route('articles.index');
+    }
+
+    public function store() {
+        $articles = Article::with('user')
+            ->latest()
+            ->paginate(5);
+
+        return view(
+            'articles.index', ['articles' => $articles,]
+        );
+    }
+
+    public function show(Article $article) {
+        return view('articles.show', ['article' => $article]);
+    }
+
+    public function edit(Article $article) {
+        return view('articles.edit', ['articles' => $article]);
+    }
+
+    public function update(Request $request, Article $article) {
+        $input = $request->validate([
+            'body' => [
+                'required',
+                'string', 
+                'max:255',
+            ],
+        ]);
+    
+        $article->body = $input['body'];
+        $article->save();
+    
+        return redirect()->route('articles.index');
+    }
+
+    public function destory(Article $article) {
+        $article->delete();
+
+        return redirect()->route('articles.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
-use App\Models\Article;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,69 +20,12 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/articles/create', function() {
-    return view('articles/create');
-})->name('articles.create');
-
-
-Route::post('/articles', function(Request $request) {
-    $input = $request->validate([
-        'body' => [
-            'required',
-            'string', 
-            'max:255',
-        ],
-    ]);
-
-    Article::create([
-        'body' => $input['body'],
-        'user_id' => Auth::id()
-    ]);
-    
-    return redirect()->route('articles.index');
-})->name('articles.index');
-
-Route::get('/articles', function(Request $request) {
-    $articles = Article::with('user')
-        ->latest()
-        ->paginate(5);
-
-    return view(
-        'articles.index', ['articles' => $articles,]
-    );
-})->name('articles.store');
-
-// Route::get('/articles/{id}', function($id) {
-//     $article = Article::find($id);
-
-//     return view('articles.show', ['article' => $article]);
-// });
-
-Route::get('/articles/{article}', function(Article $article) {
-    return view('articles.show', ['article' => $article]);
-})->name('articles.show');
-
-Route::get('articles/{article}/edit', function(Article $article) {
-    return view('articles.edit', ['articles' => $article]);
-})->name('articles.edit');
-
-Route::patch('articles/{article}', function(Request $request, Article $article) {
-    $input = $request->validate([
-        'body' => [
-            'required',
-            'string', 
-            'max:255',
-        ],
-    ]);
-
-    $article->body = $input['body'];
-    $article->save();
-
-    return redirect()->route('articles.index');
-})->name('articles.update');
-
-Route::delete('articles/{article}', function(Article $article) {
-    $article->delete();
-
-    return redirect()->route('articles.index');
-})->name('articles.delete');
+Route::controller(ArticleController::class)->group(function(){
+    Route::get('articles/create',  'create')->name('articles.create');
+    Route::post('articles',  'index')->name('articles.index');
+    Route::get('articles',  'store')->name('articles.store');
+    Route::get('articles/{article}',  'show')->name('articles.show');
+    Route::get('articles/{article}/edit',  'edit')->name('articles.edit');
+    Route::patch('articles/{article}',  'update')->name('articles.update');
+    Route::delete('articles/{article}',  'destory')->name('articles.delete');
+});
